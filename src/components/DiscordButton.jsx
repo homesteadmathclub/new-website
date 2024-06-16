a6bbed13079c384f75ac040fc2e806113f669a4d
Lior Pendler <lior.pendler@gmail.com>
2024-06-15 18:52:22 -0700

    Add configuration

--- src/components/DiscordButton.jsx
+++ src/components/DiscordButton.jsx
@@ -1,17 +1,17 @@
-"use client"
- 
-import * as React from "react"
-import { Button } from "@/components/ui/button"
-import { SiDiscord } from 'react-icons/si';
+'use client';
 
- 
-export function DiscordButton() { 
-  return (
-    <Button variant="outline" size="icon">
-      <a href="https://discord.gg/YOUR_SERVER_HERE">
-        <SiDiscord  className="text-[1.35rem] text-[#5865F2]" />
-        <span className="sr-only">Join Discord</span>
-      </a>
-    </Button>
-  )
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+import { SiDiscord } from 'react-icons/si';
+import config from '@/lib/config.json';
+
+export function DiscordButton() {
+	return (
+		<Button variant='outline' size='icon'>
+			<a href={config.discord_url}>
+				<SiDiscord className='text-[1.35rem] text-[#5865F2]' />
+				<span className='sr-only'>Join Discord Server</span>
+			</a>
+		</Button>
+	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,74 @@
-"use client"
+'use client';
 
-import Link from "next/link"
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import config from '@/lib/config.json';
+import Link from 'next/link';
+import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-} from "@/components/ui/navigation-menu"
-import { ModeToggle } from "./ModeToggle.jsx"
-import { DiscordButton } from "./DiscordButton.jsx"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+} from '@/components/ui/navigation-menu';
+import { ModeToggle } from './ModeToggle.jsx';
+import { DiscordButton } from './DiscordButton.jsx';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export function Navbar() {
-  return (
-    <nav className="list-none sticky top-0 z-50 w-full py-[0.3rem]">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="mr-4">
-          <NavigationMenu className="flex space-x-2">
-            <NavigationMenuItem className="mr-2 flex items-center justify-center"> {/* Customize club logo and name here! */}
-              <Avatar className="mr-3">
-                <AvatarImage src="/examplelogo.png" /> 
-                <AvatarFallback>HHS</AvatarFallback>
-              </Avatar>
-              <b>
-                <Link href="/" legacyBehavior passHref>
-                  Template Club
-                </Link>
-              </b>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/about" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  About
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/officers" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Officers
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/meetings" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Meetings
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenu>
-        </div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <NavigationMenu className="flex space-x-3">
-            <NavigationMenuItem>
-              <DiscordButton />
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <ModeToggle />
-            </NavigationMenuItem>
-          </NavigationMenu>
-        </div>
-      </div>
-    </nav>
-  )
+	return (
+		<nav className='list-none sticky top-0 z-50 w-full py-[0.5rem]'>
+			<div className='container flex h-20 max-w-screen-2xl items-center'>
+				<div className='mr-4'>
+					<NavigationMenu className='flex space-x-2'>
+						<NavigationMenuItem className='mr-2 flex items-center justify-center'>
+							<Avatar className='mr-3'>
+								<AvatarImage src='/logo.png' />
+								<AvatarFallback>HHS</AvatarFallback>
+							</Avatar>
+							<b>
+								<Link href='/' legacyBehavior passHref>
+									{config.club_name}
+								</Link>
+							</b>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<Link href='/about' legacyBehavior passHref>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+								>
+									About
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<Link href='/officers' legacyBehavior passHref>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+								>
+									Officers
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<Link href='/meetings' legacyBehavior passHref>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+								>
+									Meetings
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+					</NavigationMenu>
+				</div>
+				<div className='flex flex-1 items-center justify-between space-x-2 md:justify-end'>
+					<NavigationMenu className='flex space-x-3'>
+						<NavigationMenuItem>
+							<DiscordButton />
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<ModeToggle />
+						</NavigationMenuItem>
+					</NavigationMenu>
+				</div>
+			</div>
+		</nav>
+	);
 }
