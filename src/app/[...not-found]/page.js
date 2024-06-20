891fec6e21b199c9df615dcf4cdcf958a086b0d7
Luke Wang <48535279+KingSkyrock@users.noreply.github.com>
2024-06-19 17:16:41 -0700

    Add background

--- src/app/[...not-found]/page.js
+++ src/app/[...not-found]/page.js
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button"
 
 export default function Err404() {
 	return (
-		<main>
-			<div className="flex h-[70vh] flex-col items-center justify-center py-2">
+		<main className="min-h-[90dvh] w-[85%] lg:w-[80%] mx-auto lg:pt-[10dvh]">
+			<div className="flex h-[60vh] flex-col items-center justify-center py-2">
 				<main className="flex h-full flex-col items-center justify-center gap-y-8 px-20 text-center">
 					<h1 className="flex flex-col gap-y-3 text-5xl font-bold">
 						<span>
