9c6601d85aa49c953a0dcadb44e0a2557e0b7e21
Luke Wang <48535279+KingSkyrock@users.noreply.github.com>
2024-06-21 22:41:02 -0700

    Readded responsiveness

--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -5,14 +5,15 @@ import config from '@/lib/config.js';
 export default function Resources() {
 	return (
 		<main className='min-h-[90dvh] w-[85%] lg:w-[50%] mx-auto pt-[5dvh] lg:pt-[5dvh] flex flex-col pb-16'>
-			<div className='text-center items-center justify-center flex-col'>
+			<div className='text-center items-center justify-center flex-col mb-10'>
 				<h1 className='font-bold text-5xl mb-5'>Resources</h1>
-				<h1 className='font-bold text-3xl mb-10'>
-					Our email: {' '}
-					<a className='hover:underline text-blue-600 dark:text-blue-500' href={'mailto:' + config.club_email}>
-						{config.club_email}
-					</a>
-				</h1>
+				<h2 className='font-bold text-3xl'>Our email:</h2>
+				<a
+					className='text-2xl md:text-3xl md:font-bold font-semibold hover:underline text-blue-600 dark:text-blue-500 mx-auto'
+					href={'mailto:' + config.club_email}
+				>
+					{config.club_email}
+				</a>
 			</div>
 			{config.resources_sections.map((obj, index) => 
 				<div key={index}>
